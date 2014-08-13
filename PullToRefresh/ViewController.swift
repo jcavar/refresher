e39cccfbdbf8916085dc8722a536b2ef743ed0d0
Josip Cavar <josipcavar04@gmail.com>
2014-08-13 16:55:05 +0200

    Initial Commit

---
+++ PullToRefresh/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PullToRefresh
+//
+//  Created by Josip Cavar on 13/08/14.
+//  Copyright (c) 2014 Josip Cavar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
