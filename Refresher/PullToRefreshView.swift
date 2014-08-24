c664a3eeba58ff5dad2d6a9bdadcae000ea3bb8b
Josip Cavar <josipcavar04@gmail.com>
2014-08-24 12:51:46 +0200

    implemented pull to refresh animation

--- Refresher/PullToRefreshView.swift
+++ Refresher/PullToRefreshView.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import QuartzCore
 
 var KVOContext = ""
 
@@ -15,7 +16,7 @@ public class PullToRefreshView: UIView {
     var previousOffset: CGFloat = 0
     var pullToRefreshAction: (() -> ())
     var label: UILabel = UILabel()
-    var activityIndicator: UIActivityIndicatorView
+    var shapeLayer: CAShapeLayer
     var loading: Bool = false {
         
         didSet {
@@ -36,29 +37,39 @@ public class PullToRefreshView: UIView {
     override init(frame: CGRect) {
         
         pullToRefreshAction = {}
-        activityIndicator = UIActivityIndicatorView(activityIndicatorStyle:.White)
+        shapeLayer = CAShapeLayer()
         super.init(frame: frame)
-        backgroundColor = UIColor.blackColor()
+        backgroundColor = UIColor(red: 0.97, green: 0.97, blue: 0.97, alpha: 1)
         label.frame = bounds
         label.textAlignment = .Center
-        label.textColor = UIColor.whiteColor()
+        label.textColor = UIColor.blackColor()
         label.text = "pull to refresh"
         addSubview(label)
         
-        activityIndicator.hidesWhenStopped = true
-        activityIndicator.center = CGPoint(x: frame.width / 2, y: frame.height / 2)
-        addSubview(activityIndicator)
+        
+        
+        //shapeLayer.path = UIBezierPath(ovalInRect:CGRect(x: frame.height / 2 - 15, y: frame.height / 2 - 15, width: 30, height: 30)).CGPath;
+        //// Bezier Drawing
+        var bezierPath = UIBezierPath()
+        bezierPath.moveToPoint(CGPointMake(0, frame.height - 2))
+        bezierPath.addLineToPoint(CGPoint(x: frame.width, y: frame.height - 2))
+        
+        shapeLayer.path = bezierPath.CGPath
+        shapeLayer.lineWidth = 4
+
+        //[UIColor colorWithRed:0.12 green:0.49 blue:0.98 alpha:1]
+        shapeLayer.strokeColor = UIColor(red: 0.12, green: 0.49, blue: 0.98, alpha: 1).CGColor
+        shapeLayer.fillColor = UIColor.greenColor().CGColor
+        shapeLayer.strokeEnd = 0
+        //shapeLayer.strokeEnd = 0.5
+        layer.addSublayer(shapeLayer)
     }
     
     public required init(coder aDecoder: NSCoder) {
         
         pullToRefreshAction = {}
-        activityIndicator = UIActivityIndicatorView(activityIndicatorStyle:.White)
+        shapeLayer = CAShapeLayer()
         super.init(coder: aDecoder)
-
-        activityIndicator.hidesWhenStopped = true
-        activityIndicator.center = CGPoint(x: bounds.width / 2, y: bounds.height / 2)
-        addSubview(activityIndicator)
     }
     
     deinit {
@@ -83,13 +94,30 @@ public class PullToRefreshView: UIView {
                 var scrollView = object as? UIScrollView
                 if (scrollView != nil) {
                     println(scrollView?.contentOffset.y)
+                    
+                    // this should be done with tuples
+                    /*
+                    let state = (previousOffset, scrollView?.dragging, loading)
+                    switch state {
+                    
+                    case (let offset, false, false) where offset < -pullToRefreshDefaultHeight:
+                        println()
+                    }
+                    */
                     if (previousOffset < -pullToRefreshDefaultHeight) {
-                        label.text = "release to refresh"
                         if (scrollView?.dragging == false && loading == false) {
                             loading = true
+                        } else if (loading == true) {
+                            label.text = "loading ..."
+                        } else {
+                            label.text = "release to refresh"
+                            self.shapeLayer.strokeEnd = -previousOffset / pullToRefreshDefaultHeight
                         }
-                    } else {
+                    } else if (loading == true) {
+                        label.text = "loading ..."
+                    } else if (previousOffset < 0) {
                         label.text = "pull to refresh"
+                        self.shapeLayer.strokeEnd = -previousOffset / pullToRefreshDefaultHeight
                     }
                     previousOffset = scrollView!.contentOffset.y
                 }
@@ -101,7 +129,6 @@ public class PullToRefreshView: UIView {
     
     func startAnimating() {
         
-        label.alpha = 0
         var scrollView = superview as UIScrollView
         var insets = scrollView.contentInset
         
@@ -111,7 +138,25 @@ public class PullToRefreshView: UIView {
         UIView.animateWithDuration(0.3, delay: 0, options:nil, animations: {
             scrollView.contentInset = UIEdgeInsets(top: 50, left: insets.left, bottom: insets.bottom, right: insets.right)
             }, completion: {finished in
-                self.activityIndicator.startAnimating()
+                //self.activityIndicator.startAnimating()
+                
+                var pathAnimationEnd = CABasicAnimation(keyPath: "strokeEnd")
+                pathAnimationEnd.duration = 0.5
+                pathAnimationEnd.repeatCount = 100
+                pathAnimationEnd.autoreverses = true
+                pathAnimationEnd.fromValue = 0.2
+                pathAnimationEnd.toValue = 1
+                self.shapeLayer.addAnimation(pathAnimationEnd, forKey: "strokeEndAnimation")
+                
+                var pathAnimationStart = CABasicAnimation(keyPath: "strokeStart")
+                pathAnimationStart.duration = 0.5
+                pathAnimationStart.repeatCount = 100
+                pathAnimationStart.autoreverses = true
+                pathAnimationStart.fromValue = 0
+                pathAnimationStart.toValue = 0.8
+                self.shapeLayer.addAnimation(pathAnimationStart, forKey: "strokeStartAnimation")
+                
+                
                 self.pullToRefreshAction()
                 scrollView.bounces = true
         })
@@ -119,8 +164,9 @@ public class PullToRefreshView: UIView {
     
     func stopAnimating() {
         
-        activityIndicator.stopAnimating()
-        label.alpha = 1
+        //activityIndicator.stopAnimating()
+        self.shapeLayer.removeAllAnimations()
+        
         var scrollView = superview as UIScrollView
         var insets = scrollView.contentInset
         UIView.animateWithDuration(0.3, animations: {
