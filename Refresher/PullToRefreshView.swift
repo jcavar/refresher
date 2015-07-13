56fe1a71d4842c31759d9f40ba4ed7159d0eceaa
Johannes Schickling <schickling.j@gmail.com>
2015-07-13 17:41:36 +0200

    Adjust to xcode 7 beta 3

--- Refresher/PullToRefreshView.swift
+++ Refresher/PullToRefreshView.swift
@@ -86,7 +86,7 @@ public class PullToRefreshView: UIView {
         addSubview(labelTitle)
     }
     
-    public required init(coder aDecoder: NSCoder) {
+    public required init?(coder aDecoder: NSCoder) {
         
         super.init(coder: aDecoder)
         // Currently it is not supported to load view from nib
@@ -120,7 +120,7 @@ public class PullToRefreshView: UIView {
     
     //MARK: KVO methods
 
-    public override func observeValueForKeyPath(keyPath: String?, ofObject object: AnyObject?, change: [NSObject: AnyObject]?, context: UnsafeMutablePointer<()>) {
+    public override func observeValueForKeyPath(keyPath: String?, ofObject object: AnyObject?, change: [String: AnyObject]?, context: UnsafeMutablePointer<()>) {
         
         if (context == &KVOContext) {
             if let scrollView = superview as? UIScrollView where object as? NSObject == scrollView {
